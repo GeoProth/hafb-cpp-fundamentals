b8db1c90c5708e8cf3677799e1f6a08480e61477
Geoffrey Prothero <geoffreyprothero@ad.weber.edu>
2019-12-17 11:46:18 -0700

    by reference

--- Module1/TO1-4/src/age.cpp
+++ Module1/TO1-4/src/age.cpp
@@ -1,1 +1,13 @@
 #include "age.h"
+#include <iostream>
+using namespace std;
+
+void ByReferenceAgeIn5Years(int& age)
+{
+    age += 5;
+}
+
+int CurrentAge(int age)
+{
+    return age;
+}
--- Module1/TO1-4/src/age.h
+++ Module1/TO1-4/src/age.h
@@ -1,5 +1,6 @@
 #ifndef AGE_H_
 #define AGE_H_
 
-
+void ByReferenceAgeIn5Years(int& age);
+int CurrentAge(int age);
 #endif /* !AGE_H_ */
