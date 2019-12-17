ed55368f8ac772cdc1848f47ded996c2bd4bac74
Geoffrey Prothero <geoffreyprothero@ad.weber.edu>
2019-12-17 16:29:57 -0700

    read files

--- Module2/LA2-2/src/main.cpp
+++ Module2/LA2-2/src/main.cpp
@@ -1,6 +1,7 @@
-#include <array>
-#include <iostream>
-#include <string>
+#include <array>      //  for array containers
+#include <iostream>   //  basic I/O
+#include <string>     //  Strings
+#include <fstream>    //  file stream
 
 using namespace std;
 
@@ -12,49 +13,99 @@ using namespace std;
  *
  * @return int 0 means function exited with success
  */
+const int kMaxSize = 24;
 int main() {
   // create an ojbect of type ifstream
-
+  std::ifstream in; // input file stream
   // open the file age.txt
-
-
+  in.open("../age.txt"); // relative path from the executable
+  if(in.fail())
+  {
+    std::cout << "Unable to open file age.txt. \nShutting down\n";
+    return 1;
+  }
   // read from the age file and store values in an array
 
-
+  std::array<int, kMaxSize> ages;
+  ages.fill(-1); //initialize elements to -1
+  int age_size = 0;
+  while(in)
+  {
+    in >> ages[age_size];
+    age_size++;
+  }
   // print out the values in the array
+  for(auto age: ages)
+  {
+    std::cout << age << std::endl;
+  }
 
-
-  // read to the end of the file
-
-
-  // print out the valid values in the array
-
-
-  // print out the all the values in the array
+  in.close(); // close input file
 
 
   // Define an ifstream object and open the file name.txt
+  std::ifstream file;
 
-  // read one name, store it in the array, and print it
+  file.open("../name.txt");
+   if(file.fail())
+  {
+    std::cout << "Unable to open file file.txt. \nShutting down\n";
+    return 1;
+  }
 
+  std::array<string, kMaxSize> names;
+  names.fill("\0");
+  int name_size = 0;
+  while(name_size < kMaxSize && getline(file, names[name_size]))
+  {
+    //file >> names[name_size];
+    name_size++;
+  }
+  // print out the valid values in the array
 
-  // read the rest of the names in the file
+  for(auto name: names)
+  {
+    std::cout << name << std::endl;
+  }
 
-
-  // print valid elements in the names array
-
-
-  // print all the elements in the names array
-
+  file.close();
+ 
+  
 
   // Use a file with mixed values
   // Define an ifstream object and open the file name_age.txt
 
+  std::ifstream name_age;
+
+  name_age.open("../name_age.txt");
+
+ if(name_age.fail())
+  {
+    std::cout << "Unable to open file name_age.txt. \nShutting down\n";
+    return 1;
+  }
 
   // reset age and name arrays
+ std::array<string, kMaxSize> names2;
+ std::array<int, kMaxSize> ages2;
+ names2.fill("\0");
+ ages2.fill(0);
 
-
+ int num_rows = 0;
   // read all the values from the name_age.txt file
+ while(num_rows < kMaxSize && name_age >> ages2[num_rows])
+{
+  name_age.ignore();
+  std::getline(name_age, names2[num_rows]);
+  num_rows++;
+}
+
+ for(int i = 0; i < num_rows; i++)
+ {
+   std::cout << "Name: " << names2[i] << ", Age: " << ages2[i] << endl;
+ }
+
+ 
 
 
   // print valid elements in the names and ages array
