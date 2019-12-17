39eb64a3f5861c433754f353622446b0df414f4f
Geoffrey Prothero <geoffreyprothero@ad.weber.edu>
2019-12-17 14:02:21 -0700

    name spaces

--- Module1/TO1-5/src/cars.cpp
+++ Module1/TO1-5/src/cars.cpp
@@ -1,4 +1,22 @@
 #include <iostream>
 #include "cars.h"
+
+int HorsePower() // "standard" namespace
+{
+    return 150;
+}
 // using namespace lamborghini;
+int lamborghini::HorsePower()
+{
+    return 760;
+}
+void lamborghini::cout()
+{
+    std::cout << "I am a Lambo" << std::endl;
+}
+
 // using namespace porsche;
+int porsche::HorsePower()
+{
+    return 900;
+}
--- Module1/TO1-5/src/cars.h
+++ Module1/TO1-5/src/cars.h
@@ -1,5 +1,13 @@
 #ifndef CARS_H_
 #define CARS_H_
-
-
+int HorsePower();  // "nameless, ::" namespace
+namespace lamborghini
+{
+    int HorsePower();
+    void cout();
+}
+namespace porsche
+{
+    int HorsePower();
+}
 #endif /* !CARS_H_ */
