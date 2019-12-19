2328ee360e511fec2b926626e4a319e469129925
Geoffrey Prothero <geoffreyprothero@ad.weber.edu>
2019-12-19 15:29:05 -0700

    Maps, sets

--- Module5/LA5-2/src/my_map.cpp
+++ Module5/LA5-2/src/my_map.cpp
@@ -1,9 +1,51 @@
+
+
 #include <iostream>
 #include <map>
 #include <string>
 
 int main()
 {
+    // Maps are a key->value pair elements
+    // key is UNIQUE
+    // unordered
+    std::map<int, std::string> info;
 
+    // To add elements to the map, use inser(), which takes a pair of key->value
+    // to make key-value pair, us make_pair() built-in function
+
+    info.insert(std::make_pair(1, "Waldo Weber"));
+    info.insert(std::make_pair(2, "Utah Jazz"));
+    info.insert(std::make_pair(3, "Tony Tiger"));
+    info.insert(std::make_pair(4, "Hill AFB"));
+    info.insert(std::make_pair(10, "Key Value"));
+
+    std::cout << "Size of the map: " << info.size() << std::endl;
+    // Find key value
+
+    auto pos = info.find(1);
+    if(pos != info.end())
+    {
+        std::cout << "Found! The key is " << pos->first
+                << ", and the value is " << pos->second << std::endl;
+    }
+
+      // Iterate over the set
+    std::cout << "Using Iterator: " << std::endl;
+    for(auto it = std::begin(info); it != std::end(info); ++it)
+    {
+        std::cout << it->first << " " << it->second << "\t";
+    }
+        std::cout << std::endl;
+
+    //Range based
+    std::cout << "Range based" << std::endl;
+    for(std::pair<int, std::string> element : info)
+    {
+        std::cout << element.first << " " << element.second << "\t";
+    }
+        std::cout << std::endl;
+
+    
     return 0;
 }
--- Module5/LA5-2/src/my_sets.cpp
+++ Module5/LA5-2/src/my_sets.cpp
@@ -1,7 +1,43 @@
+// Elements are sorted according to the values
+// Elements are all distinct value
 #include <iostream>
-#include <set>
+#include <set>  // A container with a UNIQUE group of SORTED elements
+
+//Methods:  siz(), empty(), insert(), erase(),
+// find(): returns the position of the first element equal to the provided value
+            //you should always compare the value against the end() of the container
 
 int main()
 {
+    std::set<int> numbers;
+    numbers.insert(10);
+    numbers.insert(20);
+    numbers.insert(30);
+    numbers.insert(40);
 
+    //Search for a value
+    if(numbers.find(10) != numbers.end()) // end() is NOT the last element, the end of the set byte
+    {
+        std::cout << "10 is in numbers set" << std::endl; 
+    }
+
+    std::cout << "The set has: " << numbers.size() << " elements" << std::endl;
+
+    // Iterate over the set
+    std::cout << "Using Iterator: " << std::endl;
+    for(auto it = std::begin(numbers); it != std::end(numbers); ++it)
+    {
+        std::cout << *it << " ";
+    }
+        std::cout << std::endl;
+
+    //Range based
+    std::cout << "Range based" << std::endl;
+    for(const auto& value : numbers)
+    {
+        std::cout << value << " ";
+    }
+        std::cout << std::endl;
+
+    return 0;
 }
